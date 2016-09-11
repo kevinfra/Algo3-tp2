5daa2f6ef86ba38f2e14ea2f6c9db4d00d726822
Kevin Frachtenberg <kevinfra@users.noreply.github.com>
2016-09-11 19:43:32 -0300

    makefile y main para completar, archivos de latex en el gitignore

---
+++ src/main.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <stdio.h>      /* printf, fgets */
+#include <stdlib.h>     /* atol */
+#include <utility>
+#include <vector>
+#include <list>
+#include <string>
+#include <chrono>
+#include <random>
+
+using namespace std;
+
+static chrono::time_point<chrono::high_resolution_clock> start_time;
+
+void start_timer() {
+	start_time = chrono::high_resolution_clock::now();
+}
+
+double stop_timer() {
+	chrono::time_point<chrono::high_resolution_clock> end_time = chrono::high_resolution_clock::now();
+	return double(chrono::duration_cast<chrono::nanoseconds>(end_time - start_time).count());
+}
+
+
+int main(int argc, char *argv[]) {
+	int numeroDeEjercicio = 0;
+	bool experimentos = false;
+	if (argc == 2) {
+		numeroDeEjercicio = atoi(argv[1]);
+	}
+	else if (argc == 3) {
+		numeroDeEjercicio = atoi(argv[1]);
+		string exp = argv[2];
+		if (exp == "-exp") {
+			experimentos = true;
+		}
+	}
+	else {
+		cout << "Modo de uso: \n tp1 numeroDeEjercicio \n Opcional luego del numero de ejercicio: " << endl;
+		cout << "   -exp para experimentos" << endl;
+		return -1;
+	}
+
+
+	if (!experimentos)
+		cout << "Corriendo ejercicio numero " << numeroDeEjercicio << endl;
+
+	if (numeroDeEjercicio == 1) {
+		if (!experimentos) {
+
+		}
+		else {
+
+		}
+	}
+	else if (numeroDeEjercicio == 2) {
+		if (!experimentos) {
+
+		}
+		else {
+
+		}
+	}
+	else if (numeroDeEjercicio == 3) {
+
+		if (!experimentos){
+
+		}
+		else{
+
+		}
+	}
+	else {
+		cout << "numero de ejercicio no valido" << endl;
+		return -1;
+	}
+	return 0;
+}
+
+
+
+
