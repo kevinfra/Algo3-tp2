77ad00081015013f310458d03a0e40b732f94bc8
julianlen <julianlen@gmail.com>
2016-09-23 22:01:11 -0300

    casi terminado el 3

--- src/grafos3.h
+++ src/grafos3.h
@@ -2,38 +2,41 @@
 #include <algorithm>
 #include <vector>
 #include <stdbool.h>
+#include <tuple>
+#include <set>
+
+#define INFINITO 999999
 
 using namespace std;
 
 typedef pair<int, int> t_node; //nroNodo, peso
-
-namespace Grafos {
-    class ListAdy;
-}
-
-class ListAdy {
-
-    public:
-	    ListAdy(int estaciones, int vias, vector<ABC> recorridos);  // Constructor
-	    salida dijkstra();
-    private:
-    	int N;
-    	int M;
-    	vector< vector < t_node > > adyacencia;
-};
-
 typedef tuple<int, int, int> ABC; //Para llegar de A a B tarda C
 
-struct salida {
-	int T; //mínimo tiempo para ir de la estación 1 a N, o -1
-	int S; //cantidad de estaciones que debe recorrer
-	vector<int> escape; //S enteros que indican la forma de escapar lo más rapido posible
+namespace grafos3 {
+    struct Cmp
+    {
+        bool operator ()(const pair<int, int> &a, const pair<int, int> &b)
+        {
+            return a.first < b.first;
+        }
+    };
+
+    struct salida {
+        int T; //mínimo tiempo para ir de la estación 1 a N, o -1
+        int S; //cantidad de estaciones que debe recorrer
+        vector<int> escape; //S enteros que indican la forma de escapar lo más rapido posible
+    };
+
+    class ListAdy {
+        public:
+            ListAdy(int estaciones, int vias, vector<ABC> recorridos);  // Constructor
+            salida dijkstra();
+        private:
+            int N;
+            int M;
+            vector< vector < t_node > > adyacencia;
+    };
 }
 
-struct Cmp
-{
-    bool operator ()(const pair<int, int> &a, const pair<int, int> &b)
-    {
-        return a.first < b.first;
-    }
-};
+
+
